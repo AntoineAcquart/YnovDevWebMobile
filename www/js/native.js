e98b1ad8f8a41df49ec7cf3eec412d1e4336ac76
AntoineAcquart <antoine.acquart@gmail.com>
2021-12-10 11:24:33 +0100

    deploy on android

--- www/js/native.js
+++ www/js/native.js
@@ -2,6 +2,10 @@ function onBatteryStatus(status) {
   console.log("Battery Level Low " + status.level + "%");
 }
 
+function onOffline() {
+  navigator.notification.alert("You lost your connection !", () => { }, ["Connection lost"], ["OK"])
+}
+
 const openInAppBrowserOptions = "location=yes,zoom=false";
 
 const openInAppBrowser = (link) => {
@@ -10,4 +14,5 @@ const openInAppBrowser = (link) => {
 
 const deviceReady = () => {
   window.addEventListener("batterystatus", onBatteryStatus, false);
+  document.addEventListener("offline", onOffline, false);
 };
